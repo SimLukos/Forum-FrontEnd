667c51f8706d6a67c2cfdcfcdebc41b574c49852
Simonas Lukoševičius <simonaslukosevicius@Simonass-MacBook-Pro.local>
2023-02-27 11:11:09 +0200

    added responsive css

--- components/Title/Title.js
+++ components/Title/Title.js
@@ -2,7 +2,7 @@ import styles from "./title.module.css";
 
 export default function Title() {
   return (
-    <div>
+    <div className={styles.titleWrapper}>
       <h1 className={styles.title}>Q&A</h1>
     </div>
   );
